a63ae0ccb3bf2025e3e0c50238a3f212ac87848b
Mason Kling <klingma@oregonstate.edu>
2022-11-02 16:26:19 -0700

    Functionalized

--- feedMe.py
+++ feedMe.py
@@ -4,27 +4,29 @@ from selenium.webdriver.common.by import By
 
 import time
 
+def main():
+    driver = webdriver.Chrome()
+    driver.get('http://twitter.com/i/flow/login')
 
+    time.sleep(2)
 
-driver = webdriver.Chrome()
-driver.get('http://twitter.com/i/flow/login')
+    driver.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys('USERNAME')
 
-time.sleep(2)
+    time.sleep(2)
 
-driver.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys('USERNAME')
+    buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
+    buttons[2].click()
 
-time.sleep(2)
+    time.sleep(2)
 
-buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
-buttons[2].click()
+    driver.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys('PASSWORD')
 
-time.sleep(2)
+    time.sleep(2)
 
-driver.find_element(By.CSS_SELECTOR, "input[type='password']").send_keys('PASSWORD')
+    buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
+    buttons[2].click()
 
-time.sleep(2)
+    time.sleep(2)
 
-buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
-buttons[2].click()
-
-time.sleep(2)
+if __name__ == "__main__":
+    main()
