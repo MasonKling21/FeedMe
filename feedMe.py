ea318add525901e9c90930c7c759be5541ec8c73
Mason Kling <klingma@oregonstate.edu>
2022-11-03 12:15:51 -0700

    Added functionality

--- feedMe.py
+++ feedMe.py
@@ -8,13 +8,20 @@ def main():
     driver = webdriver.Chrome()
     driver.get('http://twitter.com/i/flow/login')
 
+    login(driver)
+
     time.sleep(2)
 
+    checkStatus(driver)
+
+def login(driver):
     driver.find_element(By.CSS_SELECTOR, "input[type='text']").send_keys('USERNAME')
 
     time.sleep(2)
 
+    ### Get all the buttons on the page
     buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
+    ### The 3rd button is the 'Next' button
     buttons[2].click()
 
     time.sleep(2)
@@ -23,10 +30,33 @@ def main():
 
     time.sleep(2)
 
+    ### Get all the buttons on the page
     buttons = driver.find_elements(By.XPATH, "//div[@role='button']")
+    ### The 3rd button is the 'Login' button
     buttons[2].click()
 
-    time.sleep(2)
+### Compares the number of likes, comments, and retweets of the most
+### recent post against the amount of followers at the time of the post
+def checkStatus(driver):
+
+    ### Give page some time to load
+    time.sleep(10)
+
+    ### Get number of followers
+    ### Store number of followers for later use
+    val = driver.find_elements(By.XPATH, "//a[@role='link']")
+
+    ### Get number of replies on last tweet
+    val = driver.find_elements(By.XPATH, "//div[@data-testid='reply']")
+
+    ### Get number of retweets on last tweet
+    val = driver.find_elements(By.XPATH, "//div[@data-testid='retweet']")
+
+    ### Get number of likes on last tweet
+    val = driver.find_elements(By.XPATH, "//div[@data-testid='like']")
+
+
+
 
 if __name__ == "__main__":
     main()
